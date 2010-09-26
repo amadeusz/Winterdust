2f7338859cdb5c799556e2a7eb1a007b6076567f
amadeusz <maciek@eldesign.eu>
2010-09-26 14:31:28 +0200

    Empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Czytnik::Application.config.secret_token = '85849235f1cb534534516847c6b60fcbf12b64627c15fc238077f178868d4f57a2d44d8dc326c5c6964a55b1d4c472eac6ab830ed2a3b5c6e600577049f6a5da'
